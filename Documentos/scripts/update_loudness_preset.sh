e8e402896b895d54b50caffb706e28788b0d8a2b
Lucas Rizzini <lucasrizzini@hotmail.com>
2023-05-27 10:11:51 -0300

    atualizando

---
+++ Documentos/scripts/update_loudness_preset.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+atualizado=0
+rm -f /tmp/LoudnessEqualizer.json
+wget https://raw.githubusercontent.com/Digitalone1/EasyEffects-Presets/master/LoudnessEqualizer.json -P /tmp/;
+if ! colordiff /tmp/LoudnessEqualizer.json /home/lucas/.config/easyeffects/output/LoudnessEqualizer.json &> /dev/null; then
+    mv -f /tmp/LoudnessEqualizer.json /home/lucas/.config/easyeffects/output/LoudnessEqualizer.json;
+    atualizado=1
+fi
+if [ $atualizado -eq 1 ]; then
+    /usr/bin/notify-send -u critical 'LoudnessEqualizer atualizado para a versão mais recente';
+else
+    /usr/bin/notify-send -u critical 'LoudnessEqualizer não atualizado';
+fi
+
+rm -f /tmp/LoudnessEqualizer.json
