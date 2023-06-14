815fa7c7151251a9d0da6a61f28599881d95b67d
Lucas Rizzini <lucasrizzini@hotmail.com>
2023-06-14 17:46:01 -0300

    atualizando

--- Documentos/scripts/taskbar_gpu_usage_temp.sh
+++ Documentos/scripts/taskbar_gpu_usage_temp.sh
@@ -9,15 +9,17 @@ fi
 # command='if [ "$(pgrep "htop")" ];then /usr/bin/killall htop;else /usr/bin/alacritty -o window.dimensions.lines=33 window.dimensions.columns=120 -e /usr/bin/htop;fi'
 command='if [ "$(pgrep "radeontop")" ];then killall radeontop &> /dev/null;else /usr/bin/alacritty -o window.dimensions.lines=30 window.dimensions.columns=120 -e /usr/bin/radeontop -Tc & disown $!;fi';
 threshold=70
+rpm_gpu_last=0
 while :; do
     cpu_temp=$(/usr/bin/sensors | grep 'edge:' | awk '{print $2}' | awk -F'[^0-9]*' '$0=$2')
     cpu_usage=$(cat /tmp/1 | tail -1 | awk '{print $5}\')
     rpm_gpu=$(sensors | grep 'fan1' | tail -1 | awk '{print $2}')
-     if [ $rpm_gpu -eq 1444 ]; then
+    if [ $rpm_gpu_last -eq $rpm_gpu ]; then
         DATA='| C | GPU: <b>'$(echo ${cpu_usage%,*})'</b> Temp: <b>'$cpu_temp'ºc / </b> FAN: <b>Off</b> | Temp: <b>'$cpu_temp'ºc</b> | '$command' |'
-     else
-         DATA='| C | GPU: <b>'$(echo ${cpu_usage%,*})'</b> Temp: <b>'$cpu_temp'ºc / </b> FAN: <b>'$rpm_gpu' RPM</b> | Temp: <b>'$cpu_temp'ºc</b> | '$command' |'
-     fi
+    else
+        DATA='| C | GPU: <b>'$(echo ${cpu_usage%,*})'</b> Temp: <b>'$cpu_temp'ºc / </b> FAN: <b>'$rpm_gpu' RPM</b> | Temp: <b>'$cpu_temp'ºc</b> | '$command' |'
+        rpm_gpu_last=0
+    fi
     if [ "$DATA" != "$DATA_last" ];then
         /usr/bin/qdbus org.kde.plasma.doityourselfbar /id_955 org.kde.plasma.doityourselfbar.pass "$DATA"
         DATA_last="$DATA"
@@ -26,4 +28,5 @@ while :; do
         > /tmp/1
     fi
     /usr/bin/sleep 1
+    rpm_gpu_last=$rpm_gpu
 done
