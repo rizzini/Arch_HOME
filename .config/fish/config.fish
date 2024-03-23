77350fdfb076c20a4de195669da57f8ad5dd695c
Lucas Rizzini <lucasrizzini@hotmail.com>
2024-03-23 09:16:10 -0300

    backup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,10 +12,10 @@ alias ls='/usr/bin/exa -al --color=always --group-directories-first';
 alias l='/usr/bin/exa -al --color=always --group-directories-first';
 alias grep='/bin/grep --color=auto';
 alias syadm="/usr/bin/sudo /usr/bin/yadm -Y /etc/yadm";
-alias bv='~/scripts/download_videos_terminal.sh'
 alias packagelist='expac --timefmt="%Y-%m-%d %T" "%l\t%n" | sort'
 alias taskbar='/home/lucas/Documentos/scripts/taskbar_cpu_usage_temp.sh & disown; /
 /home/lucas/Documentos/scripts/taskbar_disk_monitor.sh & disown; /
 /home/lucas/Documentos/scripts/taskbar_memory_usage.sh & disown; /
 /home/lucas/Documentos/scripts/taskbar_network_speed_monitor.sh & disown; /
 /home/lucas/Documentos/scripts/taskbar_volume.sh & disown'
+alias sonic='sudo tail -f /var/log/messages | /usr/bin/highlight -O ansi --syntax=Verilog | cut -c441-9999'
--- Documentos/scripts/waydroid_under_xorg.sh
+++ Documentos/scripts/waydroid_under_xorg.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 if [ "$(systemctl is-active waydroid-container.service)" == 'active' ] || lsns | grep -E 'android|lineageos';then
-    killall -9 cage &> /dev/null;
+    killall -9 weston &> /dev/null;
     sudo systemctl stop waydroid-container.service;
     sudo pkill --cgroup=/lxc.payload.waydroid;
     exit;
 fi
-killall -9 cage &> /dev/null;
+if pgrep weston; then
+    killall -9 weston;
+fi
 sudo systemctl restart waydroid-container.service;
 sleep 1;
-cage -d /usr/bin/waydroid show-full-ui
-while pgrep "cage";do
-    sleep 2;
-done
+weston --width=480 --height=600 --xwayland &> /dev/null & disown
+export WAYLAND_DISPLAY='wayland-1';
+export XDG_SESSION_TYPE="wayland";
+export DISPLAY=':1';
+sleep 1;
+alacritty -e bash -c '/usr/bin/waydroid show-full-ui'
 sudo systemctl stop waydroid-container.service;
 sudo pkill --cgroup=/lxc.payload.waydroid;
