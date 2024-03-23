dade8698d860c389882c9ee624c1b3e6560da3d3
Lucas Rizzini <lucasrizzini@hotmail.com>
2024-03-23 10:22:44 -0300

    backup

--- Documentos/scripts/aur_vote.sh
+++ Documentos/scripts/aur_vote.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-AUR_AUTO_VOTE_PASSWORD="$(gpg --decrypt /home/lucas/Documentos/scripts/aur_vote.senha.gpg)"
-export AUR_AUTO_VOTE_PASSWORD
+export AUR_AUTO_VOTE_PASSWORD="$(gpg --decrypt /home/lucas/Documentos/scripts/aur_vote.senha.gpg)"
 aur-auto-vote lucasrizzini
