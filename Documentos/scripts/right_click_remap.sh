9800ff7f2521a48ff663026a43f5dc55643b3d96
Lucas Rizzini <lucasrizzini@hotmail.com>
2024-04-02 07:19:54 -0300

    backup

--- Documentos/scripts/right_click_remap.sh
+++ Documentos/scripts/right_click_remap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export LANG=C LC_ALL=C;
-/usr/bin/xinput --test '11' | /bin/grep --line-buffered -E 'key press   108' | while read -r line; do
+/usr/bin/xinput --test '10' | /bin/grep --line-buffered -E 'key press   108' | while read -r line; do
     if [ "$line" == 'key press   108' ] && [ "$down" != '1' ]; then
         xdotool click 3 &
     fi
