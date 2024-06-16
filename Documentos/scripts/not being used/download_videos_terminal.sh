64fef098cc54e96be1009e6591914fb63186ba51
Lucas Rizzini <lucasrizzini@hotmail.com>
2024-06-16 14:41:29 +0000

    backup

--- Documentos/scripts/not being used/download_videos_terminal.sh
+++ Documentos/scripts/not being used/download_videos_terminal.sh
@@ -3,7 +3,7 @@
 url=$(/usr/bin/xclip -o -selection clipboard)
 # echo "$(tput setaf 82)URL: "$url"$(tput sgr0)"
 # regex='^https?://[^/]+'
-/usr/bin/yt-dlp "$url" -o "/mnt/hdd/Videos/%(title).200B.%(ext)s" & disown
+ yt-dlp -f "bestvsideo[height<=?720]+bestaudio/best" "$url" -o "/mnt/hdd/Videos/%(title).200B.%(ext)s"  &> /dev/null & disown
 # if [[ $url =~ $regex ]];then
 #     if ! /usr/bin/yt-dlp "$url"; then
 #         if [[ "$url" == *"reddit.com/r/"* ]];then
@@ -13,6 +13,5 @@ url=$(/usr/bin/xclip -o -selection clipboard)
 # else
 #     /bin/echo "$(tput setaf 197)URL inválido..$(tput sgr0)"
 # fi
-
 
 
