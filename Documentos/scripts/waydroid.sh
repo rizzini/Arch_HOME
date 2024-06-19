9ebca93723127a58c9a9a81b15e748c4afc40a0f
Lucas Rizzini <lucasrizzini@hotmail.com>
2024-06-19 11:22:35 -0300

    backup

---
+++ Documentos/scripts/waydroid.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ "$(systemctl is-active waydroid-container.service)" == 'active' ] || lsns | grep -E 'android|lineageos';then
+    sudo systemctl stop waydroid-container.service;
+    sudo pkill --cgroup=/lxc.payload.waydroid2;
+    sudo killall -9 lxc-start;
+    exit;
+else
+    sleep 1;
+    sudo systemctl restart waydroid-container.service;
+    waydroid show-full-ui
+fi
