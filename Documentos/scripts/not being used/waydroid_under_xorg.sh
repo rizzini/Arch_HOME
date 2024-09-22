0a17469bffad6608c58d6f2243d0a681010f5ad8
Lucas Rizzini <lucasrizzini@hotmail.com>
2024-09-22 08:21:16 -0300

    backup

--- Documentos/scripts/not being used/waydroid_under_xorg.sh
+++ Documentos/scripts/not being used/waydroid_under_xorg.sh
@@ -7,14 +7,10 @@ if [ "$(systemctl is-active waydroid-container.service)" == 'active' ] || lsns |
 else
     sudo systemctl start waydroid-container.service;
     weston --width=1024 --height=768 --xwayland &
-    export WAYLAND_DISPLAY='wayland-1';
-    export XDG_SESSION_TYPE="wayland";
-    export DISPLAY=':1';
-    sleep 1;
-    alacritty -e bash -c '/usr/bin/waydroid start session' &
+        sleep 1;
+    alacritty -e bash -c 'WAYLAND_DISPLAY='wayland-1' XDG_SESSION_TYPE="wayland"  DISPLAY=':1' /usr/bin/waydroid session start' &
+    while ! adb connect 192.168.240.112; do
+        sleep 1
+    done
     scrcpy
-    sudo systemctl stop waydroid-container.service;
-    sudo pkill --cgroup=/lxc.payload.waydroid2;
-    sudo killall -9 lxc-start;
-    killall -9 weston;
 fi
