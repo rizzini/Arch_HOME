86bee77217c9bd82fa41d457ddd5fed715b4e7cd
Lucas Rizzini <lucasrizzini@hotmail.com>
2024-10-03 08:33:45 -0300

    backup

--- Documentos/scripts/download_videos_terminal.sh
+++ Documentos/scripts/download_videos_terminal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 gui="$(loginctl show-session $(awk '/tty/ {print $1}' <(loginctl)) -p Type | awk -F= '{print $2}')";
-    url=$(xclip -o);
+    url=$(xclip -o | egrep -o 'https?://[^ ]+');
 
 if ! yt-dlp -f "bestvsideo[height<=?1080]+bestaudio/best" "$url" -o "/mnt/hdd/Videos/%(title).200B.%(ext)s(ext)s"  &> /dev/null; then
     /usr/bin/notify-send 'URL nao suportado' -t 2000;
