869eac0aa399bfecf514b2ebd964f25f3f208098
Lucas Rizzini <lucasrizzini@hotmail.com>
2024-11-03 09:51:52 -0300

    backup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,7 +10,7 @@ end
 alias plasmashell='/usr/bin/killall plasmashell &> /dev/null; /usr/bin/plasmashell --replace &> /dev/null & disown $last_pid';
 alias ls='/usr/bin/exa -al --color=always --group-directories-first --icons';
 alias l='/usr/bin/exa -al --color=always --group-directories-first --icons';
-alias grep='/bin/grep --color=auto';
+#alias grep='/bin/grep --color=auto';
 alias syadm="/usr/bin/sudo /usr/bin/yadm -Y /etc/yadm";
 alias packagelist='expac --timefmt="%Y-%m-%d %T" "%l\t%n" | sort'
 bind \b backward-kill-path-component
