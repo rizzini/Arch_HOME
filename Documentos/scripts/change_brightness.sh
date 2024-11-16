e85083aeaf3715d207fe7ececd2097e6742cecf7
Lucas Rizzini <lucasrizzini@hotmail.com>
2024-11-16 08:57:50 -0300

    backup

--- Documentos/scripts/change_brightness.sh
+++ Documentos/scripts/change_brightness.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-current=$(ddcutil getvcp color | awk '/\<'"0x10"'\>/{print $9}' | tr -d ',');
+if pgrep zenity; then
+    exit;
+fi
+if [ ! -f '/home/lucas/Documentos/scripts/change_brightness.db' ]; then
+	current=$(ddcutil getvcp color | awk '/\<'"0x10"'\>/{print $9}' | tr -d ',');
+else
+	current="$(cat /home/lucas/Documentos/scripts/change_brightness.db)"
+fi 
 case $1 in
 'aumentar')
         i=$((current + 5))
@@ -22,9 +29,12 @@ case $1 in
         fi
         ;;
 'escolher')
-		i=$(yad --title="Choose a value"  --scale --value=50)
-            ddcutil setvcp 0x10 "$i";
-            ddcutil setvcp 0x12 "$i";
+		if i=$(LC_ALL=C zenity --scale --value="$current" --step=3); then
+			ddcutil setvcp 0x10 "$i";
+			ddcutil setvcp 0x12 "$i";
+			echo "$i" | tee /home/lucas/Documentos/scripts/change_brightness.db
+        fi
+            
 		;;
 ''|*[0-9]*)
         if [ "$1" -le 100 ] && [ "$1" -ge 0 ]; then
