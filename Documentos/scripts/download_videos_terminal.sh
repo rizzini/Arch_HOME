e037128763cda2b6d7952440b97fd560471733bf
Lucas Rizzini <lucasrizzini@hotmail.com>
2024-11-24 19:20:55 -0300

    backup

--- Documentos/scripts/download_videos_terminal.sh
+++ Documentos/scripts/download_videos_terminal.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 gui="$(loginctl show-session $(awk '/tty/ {print $1}' <(loginctl)) -p Type | awk -F= '{print $2}')";
+
+if [[ "$(loginctl show-session $(loginctl | grep "$USER" | awk '{print $1}') -p Type)" == *"wayland"* ]]; then
+    url=$(wl-paste | egrep -o 'https?://[^ ]+');
+else
     url=$(xclip -o | egrep -o 'https?://[^ ]+');
+fi
 
 if ! yt-dlp -N 10 -f "bestvsideo[height<=?1080]+bestaudio/best" "$url" -o "/mnt/hdd/Videos/xxx/new/%(title).200B.%(ext)s(ext)s"  &> /dev/null; then
     /usr/bin/notify-send 'URL nao suportado' -t 2000;
