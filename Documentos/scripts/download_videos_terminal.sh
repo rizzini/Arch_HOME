d89eb5fb66a81c0cb946ce7f525be8f89afd47e0
Lucas Rizzini <lucasrizzini@hotmail.com>
2024-11-30 10:15:53 -0300

    backup

--- Documentos/scripts/download_videos_terminal.sh
+++ Documentos/scripts/download_videos_terminal.sh
@@ -9,7 +9,7 @@ fi
 
 if ! yt-dlp -N 10 -f "bestvsideo[height<=?1080]+bestaudio/best" "$url" -o "/mnt/hdd/Videos/xxx/new/%(title).200B.%(ext)s(ext)s"  &> /dev/null; then
     /usr/bin/notify-send 'URL nao suportado' -t 2000;
+    echo "$url" | tee -a /home/lucas/error.txt
 else
-#     /usr/bin/notify-send 'ok' -t 5000;
-    echo 'ok'
+    echo "$url" | tee -a /home/lucas/.xxx_ok.txt
 fi
